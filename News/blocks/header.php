c20b82dc0252a3c77461126a0086b4eb7b4dc95d
MartineZof1 <73905096+MartineZof1@users.noreply.github.com>
2022-03-22 11:33:41 +0200

    Add files via upload

---
+++ News/blocks/header.php
@@ -0,0 +1,21 @@
+<header>
+	<div id="logo">
+		<a href="index.php" title="На главную"><span>Н</span>овости ц."<span>С</span>лово <span>Б</span>ожье"</a>
+	</div>
+	<div id="menuHead">
+		<a href="about.php">
+				<div style="margin-right: 15%"> О нас</div>
+		</a>
+		<a href="feedback.php">
+				<div>Связаться с нами</div>
+		</a>
+	</div>
+	<div id="regAuth">
+		<a href="auth.php">
+			<div style="margin-left: 3%">Вход</div>
+		</a>
+		<a href="registration.php">
+			<div>Регистрация</div>
+		</a>
+	</div>
+</header>
